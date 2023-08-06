3789f857850d4f8fdb586e6ac2c7a009c84014a2
mehmet_gur <mehmet.gur@getir.com>
2023-08-07 01:09:02 +0300

    Initial commit

---
+++ shared/build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    kotlin("multiplatform")
+    id("com.android.library")
+}
+
+@OptIn(org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi::class)
+kotlin {
+    targetHierarchy.default()
+
+    android {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "1.8"
+            }
+        }
+    }
+    
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = "shared"
+        }
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation("com.squareup.sqldelight:runtime:1.5.3")
+                implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+    }
+}
+
+android {
+    namespace = "com.example.noteappkmm"
+    compileSdk = 33
+    defaultConfig {
+        minSdk = 24
+    }
+}
