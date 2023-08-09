987753d8c13ad743aab07127db0012e3163604f0
mehmet_gur <mehmet.gur@getir.com>
2023-08-09 16:22:12 +0300

    NoteDetailViewModel implemented

--- androidApp/src/main/java/com/example/noteappkmm/android/note_detail/NoteDetailState.kt
+++ androidApp/src/main/java/com/example/noteappkmm/android/note_detail/NoteDetailState.kt
@@ -1,11 +1,9 @@
 package com.example.noteappkmm.android.note_detail
 
-import androidx.compose.ui.graphics.Color
-
 data class NoteDetailState(
     val noteTitle: String = "",
-    val isNoteTitleTextFocused: Boolean = false,
+    val isNoteTitleHintVisible: Boolean = false,
     val noteContent: String = "",
-    val isNoteContentTextFocused: Boolean = false,
-    val noteColor: Color = Color.White
+    val isNoteContentHintVisible: Boolean = false,
+    val noteColor: Long = 0xFFFFFF
 )
---
+++ androidApp/src/main/java/com/example/noteappkmm/android/note_detail/NoteDetailViewModel.kt
@@ -0,0 +1,101 @@
+package com.example.noteappkmm.android.note_detail
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.noteappkmm.domain.DateTimeUtil
+import com.example.noteappkmm.domain.Note
+import com.example.noteappkmm.domain.NoteDataSource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class NoteDetailViewModel @Inject constructor(
+    private val noteDataSource: NoteDataSource,
+    private val savedStateHandle: SavedStateHandle
+) : ViewModel() {
+
+    private val noteTitle = savedStateHandle.getStateFlow("noteTitle", "")
+    private val isNoteTitleFocused = savedStateHandle.getStateFlow("isNoteTitleFocused", false)
+    private val noteContent = savedStateHandle.getStateFlow("noteContent", "")
+    private val isNoteContentFocused = savedStateHandle.getStateFlow("isNoteContentFocused", false)
+    private val noteColor = savedStateHandle.getStateFlow(
+        "noteColor",
+        Note.generateColors()
+    )
+
+    val state = combine(
+        noteTitle,
+        isNoteTitleFocused,
+        noteContent,
+        isNoteContentFocused,
+        noteColor
+    ) { title, isTitleFocused, content, isContentFocused, color ->
+        NoteDetailState(
+            noteTitle = title,
+            isNoteTitleHintVisible = title.isEmpty() && isTitleFocused.not(),
+            noteContent = content,
+            isNoteContentHintVisible = content.isEmpty() && isContentFocused.not(),
+            noteColor = color
+        )
+    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), NoteDetailState())
+
+    private val _isNoteSaved = MutableStateFlow(false)
+    val isNoteSaved = _isNoteSaved.asStateFlow()
+
+    private var existingNoteId: Long? = null
+
+    init {
+        savedStateHandle.get<Long>("noteId")?.let { currentNoteId ->
+            if (existingNoteId == -1L) {
+                return@let
+            }
+            existingNoteId = currentNoteId
+            viewModelScope.launch {
+                noteDataSource.getNote(existingNoteId ?: 0)?.let { note ->
+                    savedStateHandle["noteTitle"] = note.title
+                    savedStateHandle["noteContent"] = note.content
+                    savedStateHandle["noteColor"] = note.colorHex
+                }
+            }
+        }
+    }
+
+    fun onNoteTitleChanged(text: String) {
+        savedStateHandle["noteTitle"] = text
+    }
+
+    fun onNoteContentChanged(text: String) {
+        savedStateHandle["noteContent"] = text
+    }
+
+    fun onNoteTitleFocusChanged(isFocused: Boolean) {
+        savedStateHandle["isNoteTitleFocused"] = isFocused
+    }
+
+    fun onNoteContentFocusChanged(isFocused: Boolean) {
+        savedStateHandle["isNoteContentFocused"] = isFocused
+    }
+
+    fun saveNote() {
+        viewModelScope.launch {
+            noteDataSource.insertNote(
+                Note(
+                    id = existingNoteId,
+                    title = noteTitle.value,
+                    content = noteContent.value,
+                    colorHex = noteColor.value,
+                    created = DateTimeUtil.now()
+                )
+            )
+            _isNoteSaved.value = true
+        }
+    }
+
+}
