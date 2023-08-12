93f85818b6d60fa99ee9951c764bd687ac377d12
mehmet_gur <mehmet.gur@getir.com>
2023-08-12 04:02:56 +0300

    [ios] NoteListViewModel.swift implemented

---
+++ iosApp/iosApp/note_list/NoteListScreen.swift
@@ -0,0 +1,21 @@
+//
+//  NoteListScreen.swift
+//  iosApp
+//
+//  Created by Mehmet Gür on 12.08.2023.
+//  Copyright © 2023 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct NoteListScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NoteListScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        NoteListScreen()
+    }
+}
--- iosApp/iosApp/note_list/NoteListViewModel.swift
+++ iosApp/iosApp/note_list/NoteListViewModel.swift
@@ -7,3 +7,51 @@
 //
 
 import Foundation
+import shared
+
+extension NoteListScreen {
+    @MainActor class NoteListViewModel: ObservableObject {
+        private var noteDataSource: NoteDataSource? = nil
+        
+        private let searchNotesUseCase = SearchNotesUseCase()
+        
+        private var notes = [Note]()
+        @Published private(set) var filteredNotes = [Note]()
+        @Published var searchText = "" {
+            didSet {
+                self.filteredNotes = searchNotesUseCase.execute(notes: self.notes, query: searchText)
+            }
+        }
+        @Published private(set) var isSearchActive = false
+        
+        init(noteDataSource: NoteDataSource? = nil) {
+            self.noteDataSource = noteDataSource
+        }
+        
+        func loadNotes() {
+            noteDataSource?.getAllNotes(completionHandler: { notes, error in
+                self.notes = notes ?? []
+                self.filteredNotes = self.notes
+            })
+        }
+        
+        func deleteNote(id: Int64?) {
+            if id != nil {
+                noteDataSource?.deleteNote(id: id!, completionHandler: { error in
+                    self.loadNotes()
+                })
+            }
+        }
+        
+        func toggleIsSearchActive() {
+            isSearchActive = !isSearchActive
+            if !isSearchActive {
+                searchText = ""
+            }
+        }
+        
+        func setNoteDataSource(dataSource: NoteDataSource) {
+            self.noteDataSource = dataSource
+        }
+    }
+}
